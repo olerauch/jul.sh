f59d8fe9fe85ad693445c71c17a0da4eb7634387
Ole Rauch <17141836+olerauch@users.noreply.github.com>
2020-08-01 21:55:32 +0200

    Update index.tsx

--- src/components/GlobalStyles/index.tsx
+++ src/components/GlobalStyles/index.tsx
@@ -5,8 +5,8 @@ const GlobalStyles = createGlobalStyle`
     font-size: 24px;
 
     /* fonts */
-    --primaryFont: Juliette, monospace;
-    --sansFont: 'Juliette Sans', sans-serif;
+    --primaryFont: Neue Kabel-Regular, monospace;
+    --sansFont: 'Neue Kabel-Regular', sans-serif;
 
     /* colors */
     --background-color: hsl(210, 3%, 85%);
